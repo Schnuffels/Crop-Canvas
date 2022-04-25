7bdf310ef9d03e07099a77d5818e8065de968477
kinoko <943771598@qq.com>
2022-04-26 02:36:32 +0800

    修复一些问题，支持配置初始参数，第一个成型版本

---
+++ README.md
@@ -0,0 +1,60 @@
+# CorpCanvas
+画布裁剪，去除周围透明像素点
+
+## 使用方法
+
+> 说明
+
+* 本库用于去除 Canvas 图形外围透明像素点 （默认，可指定要去除的 rgba 颜色）
+
+> 创建一个裁切对象
+
+* canvasObj 为你的 canvas 画布对象
+* 参数 2 可传入以下对象，如无需求可以不进行配置
+
+```javascript
+//创建 Canvas 裁切对象
+let crop = new CropCanvas(canvasObj, {
+    scanX: 1,           // 初始扫描 X 轴像素点（与 scanY 形成矩阵块，值越大扫描越快）
+    scanY: 1,           // 初始扫描 Y 轴像素点（与 scanX 形成矩阵块，值越大扫描越快）
+    rgba: '0,0,0,0',    // 初始需要被裁切的 rgba 值
+    edge: 1             // 初始图形距离边界的留空距离
+});
+```
+
+
+## 调用方法
+
+这里说明调用方法的规范
+
+>  analysis() 分析
+
+* analysis 方法是对 canvas 画布进行线性扫描的过程
+* 参数1：true | false 可以红色像素填充准备去除的边界（支持链式调用）
+* 参数2：true | false 开启扫描动画，用户可以观看扫描的效果（不支持链式调用，且无实质效果，无法导出与裁剪，仅播放扫描动画）
+
+```javascript
+crop.analysis(/*true*/ /*,true*/)
+```
+
+>  appendTo() 将裁切后的图片载入页面
+
+* id 为 标签的 id 或者 class，如 #test 或 .test, 未设置默认载入 body
+
+```javascript
+// 请注意 appendTo() 方法不支持单独调用，必须先调用 analysis();
+crop.analysis(/*true*/).appendTo();
+```
+>  export() 导出裁切后的图片
+
+* 参数1：文件名
+
+```javascript
+// 请注意 export() 方法不支持单独调用，必须先调用 analysis();
+crop.analysis(/*true*/).export(/*filename*/);
+```
+
+
+## 总结
+
+* 本插件支持对 canvas 图形外边距进行裁切处理，并导出或者展示图像
--- cropCanvas.js
+++ cropCanvas.js
@@ -1,34 +1,54 @@
 // Crop an empty area of the canvas
 (function () {
 
-    let pendingCanvas;
-    let pendingImgData;
-    let finalCanvas;
+    let scanSizeX = 1;
+    let scanSizeY = 1;
+    let edge = 1;
+    let tR = 0;
+    let tG = 0;
+    let tB = 0;
+    let tA = 0;
 
-    function CropCanvas(canvasElement){
+    function CropCanvas(canvasElement, obj){
         this.pendingCanvas = canvasElement;
+        if(typeof(obj) !== 'undefined'){
+            scanSizeX = obj.scanX == null ? 1 : obj.scanX;
+            scanSizeY = obj.scanY == null ? 1 : obj.scanY;
+            edge = obj.edge == null ? 1 : obj.edge;
+            let rgba = [];
+            rgba = obj.rgba == null ? [0, 0, 0, 0] : obj.rgba.replace(/\s*/g, '').split(',');
+            if(rgba.length == 4){
+                tR = parseInt(rgba[0]);
+                tG = parseInt(rgba[1]);
+                tB = parseInt(rgba[2]);
+                tA = parseInt(rgba[3]);
+            }
+        }
     }
 
-    CropCanvas.prototype.analysis = function(displayBoundary = false){
+    CropCanvas.prototype.analysis = function(displayBoundary = false, animatePlayback = false){
+
+        let ctx = this.pendingCanvas.getContext('2d');
         // 存储非透明元素的所有像素点
         let contentPosition = [];
 
-        console.log(this.pendingCanvas.width)
-        console.log(this.pendingCanvas.height)
+        if(animatePlayback){
+            this.analysisAnimation(ctx);
+            return;
+        }
 
-        for (let i = 0; i < this.pendingCanvas.width; i += 100) {
-            for (let j = 0; j < this.pendingCanvas.height; j += 100) {
+        for (let i = 0; i < this.pendingCanvas.width; i += scanSizeX) {
+            for (let j = 0; j < this.pendingCanvas.height; j += scanSizeY) {
 
-                let positionColor = ctx.getImageData(i, j, 100, 100)
+                let positionColor = ctx.getImageData(i, j, scanSizeX, scanSizeY)
                 let r = positionColor.data[0];
                 let g = positionColor.data[1];
                 let b = positionColor.data[2];
                 let a = positionColor.data[3];
 
                 // 如果是透明像素点
-                if(!(r === 255 && g === 255 && b === 255 && a === 255)){
+                if(!(r === tR && g === tG && b === tB && a === tA)){
                     contentPosition.push({x:i, y:j})
-
                     // console.log('R:' + r, ' G:' + g, ' B:' + b, ' A:' + a)
                 }
                 
@@ -39,21 +59,20 @@
         let rOffset = 0;    //最右偏移
         let tOffset = 0;    //最上偏移
         let bOffset = 0;    //最下偏移
-        let paddingOffset = 100;  //距离裁切边距
 
         // 分析坐标
         lOffset = contentPosition.sort((a, b) => {
             return a.x - b.x
-        })[0].x - paddingOffset;
+        })[0].x - edge;
         rOffset = contentPosition.sort((a, b) => {
             return b.x - a.x
-        })[0].x + paddingOffset;
+        })[0].x + edge;
         tOffset = contentPosition.sort((a, b) => {
             return a.y - b.y
-        })[0].y - paddingOffset;
+        })[0].y - edge;
         bOffset = contentPosition.sort((a, b) => {
             return b.y - a.y
-        })[0].y + paddingOffset;
+        })[0].y + edge;
 
         // 继续分析出边界，或者在此停止
         if(displayBoundary){
@@ -68,25 +87,88 @@
         }
 
         this.pendingImgData = ctx.getImageData(lOffset, tOffset, rOffset - lOffset, bOffset - tOffset);
+
         return this;
     }
 
-    CropCanvas.prototype.appendToBody = function(){
+    CropCanvas.prototype.analysisAnimation = function(ctx){
+        let contentPosition = [];
+        let i = 0;
+        iFor(this, ctx, i, contentPosition);
+    }
+ 
+
+    function iFor(tThis, ctx, i, contentPosition){
+        if(i < tThis.pendingCanvas.width){
+            jFor(tThis, ctx, i, contentPosition);
+        }
+    }
+
+    function jFor(tThis, ctx, i, contentPosition){
+        let j = 0;
+        let jTimer = setInterval(() => {
+
+            if(j < tThis.pendingCanvas.height){
+
+                let positionColor = ctx.getImageData(i, j, scanSizeX, scanSizeY)
+                let r = positionColor.data[0];
+                let g = positionColor.data[1];
+                let b = positionColor.data[2];
+                let a = positionColor.data[3];
+
+                // 如果是透明像素点
+                if(!(r === tR && g === tG && b === tB && a === tA)){
+                    ctx.fillStyle = 'white';
+                    ctx.fillRect(i, j, 1, 1)
+                    contentPosition.push({x:i, y:j})
+                } else {
+                    ctx.fillStyle = 'red';
+                    ctx.fillRect(i, j, 1, 1)
+                }
+
+                j += scanSizeY;
+            } else {
+                clearInterval(jTimer);
+                iFor(tThis, ctx, i += scanSizeX, contentPosition);
+            }
+
+        }, 1)
+    }
+
+    CropCanvas.prototype.appendTo = function(id = null){
+        if(typeof(this.pendingImgData) !== 'undefined'){
+            this.putToFinalCanvas();
+            if(id != null){
+                document.querySelector(id).appendChild(this.finalCanvas)
+            } else {
+                document.body.appendChild(this.finalCanvas);
+            }
+        } else {
+            console.log('裁切图片未能载入页面，请先调用 analysis()')
+        }
+        return this;
+    }
+
+    CropCanvas.prototype.putToFinalCanvas = function(){
         this.finalCanvas = document.createElement('canvas');
         let _ctx = this.finalCanvas.getContext('2d');
         this.finalCanvas.id = "final";
         this.finalCanvas.width = this.pendingImgData.width;
         this.finalCanvas.height = this.pendingImgData.height;
         _ctx.putImageData(this.pendingImgData, 0, 0);
-
-        document.body.appendChild(this.finalCanvas);
-
-        return this;
+        this.putToCanvasStatus = true;
     }
 
     CropCanvas.prototype.export = function(filename){
+        if(typeof(this.pendingImgData) === 'undefined'){
+            console.log('裁切图片未能导出，请先调用 analysis()')
+            return;
+        }
         if(filename == null){
             filename = 'test.png';
+        }
+        if(!this.putToCanvasStatus){
+            this.putToFinalCanvas();
         }
         // 图片导出为 png 格式
         this.pendingImgData = this.finalCanvas.toDataURL("image/png");
